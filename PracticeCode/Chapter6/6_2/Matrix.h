74f737916887d7c932da7edc740c62a951f1bb7f
Lyb-code <lyb62051169@163.com>
2022-06-13 16:27:01 +0800

    Friend function declarations are to be template functions & Template class declarations and definitions must be placed together in the header file!

--- PracticeCode/Chapter6/6_2/Matrix.h
+++ PracticeCode/Chapter6/6_2/Matrix.h
@@ -3,8 +3,11 @@ using namespace std;
 template <typename elemType>
 class Matrix
 {
-    friend Matrix<elemType> operator+(const Matrix<elemType>&, const Matrix<elemType>&);
-    friend Matrix<elemType> operator*(const Matrix<elemType>&, const Matrix<elemType>&);
+    //类模板中友函数声明要为模板函数，注意不要用和Matrix的模板参数名称一样的模板参数名
+    template <typename T>
+    friend Matrix<T> operator+(const Matrix<T>&, const Matrix<T>&);
+    template <typename T>
+    friend Matrix<T> operator*(const Matrix<T>&, const Matrix<T>&);
 
 public:
     Matrix(int rows, int columns)
@@ -41,6 +44,78 @@ protected:
 };
 
 template <typename elemType>
-inline ostream& 
-operator<< (ostream &os, const Matrix<elemType> &m)
+inline ostream& operator<<(ostream &os, const Matrix<elemType> &m)
     { return m.print(os); }
+
+template<typename elemType>
+Matrix<elemType>::Matrix(const Matrix &rhs)
+{
+    _rows = rhs._rows; _cols = rhs._cols;
+    int mat_size = _rows*_cols;
+    _matrix = new elemType[mat_size];
+    for (int i = 0; i < mat_size; ++i)
+        _matrix[i] = rhs._matrix[i];
+}
+
+template<typename elemType>
+Matrix<elemType>& Matrix<elemType>::operator=(const Matrix &rhs)
+{
+    if (this != &rhs) {
+        _rows = rhs._rows; _cols = rhs._cols;
+        int mat_size = _rows*_cols;
+        delete []_matrix;
+        _matrix = new elemType[mat_size];
+        for (int i = 0; i < mat_size; ++i)
+            _matrix[i] = rhs._matrix[i];
+    }
+    return *this;
+}
+
+template<typename elemType>
+Matrix<elemType>
+operator+(const Matrix<elemType> &m1, const Matrix<elemType> &m2)
+{   
+    Matrix<elemType> result(m1);
+    result += m2;
+    return result;
+}
+
+
+template<typename elemType>
+Matrix<elemType>
+operator*(const Matrix<elemType> &m1, const Matrix<elemType> &m2)
+{
+    Matrix<elemType> result( m1.rows(),m2.cols());
+    for (int i = 0; i < m1.rows(); i++) {
+        for (int j = 0; j < m2.cols(); j++) {
+            result(i, j) = 0;
+            for (int k = 0; k < m1.cols(); k++) {
+                result(i, j) += m1(i, k) * m2(k, j);
+            }
+        }
+    }
+    return result;
+
+}
+
+template <typename elemType>
+void Matrix<elemType>::operator+=( const Matrix &m )
+{
+    // 确定ml和 m2的大小相同
+    int matrix_size = cols()*rows();
+    for(int ix=0;ix<matrix_size;++ix)
+        (*( _matrix+ ix))+=(*(m._matrix+ ix));
+}
+
+template <typename elemType>
+ostream& Matrix<elemType>::print( ostream &os) const
+{
+    int col = cols();
+    int matrix_size = col*rows();
+    for(int ix= 0;ix< matrix_size;++ix ){
+        if(ix % col == 0)os << endl;
+        os << (*(_matrix+ ix)) << ' ';
+    }
+    os << endl;
+    return os;
+}
--- PracticeCode/Chapter6/6_2/main.cpp
+++ PracticeCode/Chapter6/6_2/main.cpp
@@ -3,42 +3,58 @@
 #include "Matrix.h"
 using namespace std;
 
-int main() {
+int main()
+{
     ofstream log("..\\log.txt");
-    if(!log )
-        { cerr << "can't open log file!\n"; return -1; }
+    if(!log)
+    {
+        cerr << "can't open log file!\n";
+        return 0;
+    }
 
-    Matrix<float> identity(4,4);
-    log << "identity:" << identity << endl;
-    float ar[16]={1.,0.,0.,0.,0.,1.,0.,0.,
-                    0.,0.,1.,0.,0.,0.,0.,1.};
-    for(int i=0,k=0;i < 4;++i)
-        for( int j= 0;j<4;++j)
-            identity( i,j)= ar[ k++ ];
-    log << "identity after set:"<< identity << endl;
+    Matrix<float> identity(4, 4);
+    cout << "identity: " << identity << endl;
+    float ar[16] = {1., 0., 0., 0., 0., 1., 0., 0.,
+                    0., 0., 1., 0., 0., 0., 0., 1. };
 
-    Matrix<float> m(identity); 
-    log << "m: memberwise initialized:"<< m << endl;
+    for(int i = 0, k = 0; i < 4; ++i)
+    {
+        for(int j = 0; j < 4; ++j)
+        {
+            identity(i, j) = ar[k++];
+        }
+    }
+    cout << "identity after set: " << identity << endl;
 
-    Matrix<float> m2(8,12); 
-    log << "m2: 8x12: "<< m2 << endl;
+    Matrix<float> m(identity);
+    cout << "m: memberwise initialized: " << m << endl;
+
+    Matrix<float> m2(8, 12);
+    cout << "m2: 8x12: " << m2 << endl;
 
     m2 = m;
-    log <<"m2 after memberwise assigned to m: "
-        << m2<< endl;
+    cout << "m2 after memberwise assigned to m: " << m2 << endl;
 
-    float ar2[16]={11.3,0.4,2.6,8.2,6.2,1.7,1.3,8.3,
-                    4.2,7.4,2.7,1.9,6.3,8.1,5.6,6.6};
-    Matrix<float> m3( 4,4);
-    for(int ix= 0,kx= 0;ix< 4;++ix)
-        for(int j= 0;j<4;++j)
-            m3( ix,j)= ar2[ kx++ ];
+    float ar2[16] = {1.3, 0.4, 2.6, 8.2, 6.2, 1.7, 1.3, 8.3,
+                     4.2, 7.4, 2.7, 1.9, 6.3, 8.1, 5.6, 6.6 };
 
-    log <<"m3:assigned random values:"<< m3 << endl;
-    
-    Matrix<float> m4 = m3* identity;log << m4 << endl; 
-    Matrix<float>m5=m3 + m4;log << m5 << endl;
-    m3 += m4; log << m3 << endl;
+    Matrix<float> m3(4,4);
+    for(int i = 0, k = 0; i < 4; ++i)
+    {
+        for(int j = 0; j < 4; ++j)
+        {
+            m3(i, j) = ar2[k++];
+        }
+    }
+    cout << "m3: assigned random values: " << m3 << endl;
+
+    Matrix<float> m4 = m3*identity;
+    cout << "result of m3*identity: " << m4 << endl;
+
+    Matrix<float> m5 = m3 + m4;
+    cout << "result of m3 + m4: " << m5 << endl;
+
+    m3 += m4;
+    cout << "m3: plus with m4: " << m3 << endl;
     system("pause");
-    return 0;
 }
